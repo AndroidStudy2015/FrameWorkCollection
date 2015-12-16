22077a45e8f2b8c700e305e21714b7112bd82fcb
AndroidStudy2015 <android_study_2015@163.com>
2015-12-16 20:52:17 +0800

    因为GET/POST可能要增加多个请求头，所以将多个请求头封装在Map<String, String>headers，通过 for (Map.Entry<String, String> entry : headers.entrySet()) { 			connection.addRequestProperty(entry.getKey(), entry.getValue()); 		} 来方便地增加请求头

--- src/com/example/FrameWorkCollection/http/HttpUrlConnectionUtil.java
+++ src/com/example/FrameWorkCollection/http/HttpUrlConnectionUtil.java
@@ -7,6 +7,7 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
 
 import org.apache.http.HttpStatus;
 
@@ -20,15 +21,19 @@ public class HttpUrlConnectionUtil {
 	 * HttpURLConnection:GET请求的基本写法
 	 * 
 	 * @param url
+	 * @param headers
+	 *            需要增加的请求头headers，多个请求头存在map集合里
 	 * @return
 	 * @throws IOException
 	 */
-	public static String get(String url) throws IOException {
+	public static String get(String url,Map<String, String>headers) throws IOException {
 		HttpURLConnection connection = (HttpURLConnection) new URL(url)
 				.openConnection();
 		connection.setRequestMethod("GET");
 		connection.setConnectTimeout(15 * 3000);
 		connection.setReadTimeout(15 * 3000);
+		
+		addHeader(headers, connection)
 
 		int status = connection.getResponseCode();
 		if (status == HttpStatus.SC_OK) {
@@ -53,10 +58,13 @@ public class HttpUrlConnectionUtil {
 	 * @param url
 	 * @param content
 	 *            在这里传递参数，键值对形式
+	 * @param headers
+	 *            需要增加的请求头headers，多个请求头存在map集合里
 	 * @return
 	 * @throws IOException
 	 */
-	public static String post(String url, String content) throws IOException {
+	public static String post(String url, String content,
+			Map<String, String> headers) throws IOException {
 		HttpURLConnection connection = (HttpURLConnection) new URL(url)
 				.openConnection();
 		connection.setRequestMethod("POST");
@@ -64,10 +72,12 @@ public class HttpUrlConnectionUtil {
 		connection.setReadTimeout(15 * 3000);
 		connection.setDoOutput(true);
 
-		// 以下这两行为POST和GET的不同之处：
 		// 记得加一个请求头header
-		connection.addRequestProperty("content-type", "application/json");
-		// 把content数据传到服务器的步骤（请求体传参）
+		// connection.addRequestProperty("content-type", "application/json");
+		// ★★★★★请求头有很多，这里在方法里传入一个map参数，键值对增加请求头
+		addHeader(headers, connection);
+		// 以下这两行为POST和GET的不同之处：
+		// ★把content数据传到服务器的步骤（请求体传参）
 		OutputStream os = connection.getOutputStream();
 		os.write(content.getBytes());
 
@@ -90,4 +100,20 @@ public class HttpUrlConnectionUtil {
 
 	}
 
+	/**
+	 * ★★★★★增加请求头
+	 * 
+	 * @param headers
+	 * @param connection
+	 */
+
+	private static void addHeader(Map<String, String> headers,
+			HttpURLConnection connection) {
+		if (headers==null||headers.size()=0) {
+			return;
+		}
+		for (Map.Entry<String, String> entry : headers.entrySet()) {
+			connection.addRequestProperty(entry.getKey(), entry.getValue());
+		}
+	}
 }
